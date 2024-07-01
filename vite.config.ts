1ecaaedab28092795260af0befbe4c4253d4123c
Léandre <leandre.sentenac@epitech.eu>
2024-07-01 12:05:50 +0200

    Add shadcn

--- vite.config.ts
+++ vite.config.ts
@@ -1,3 +1,4 @@
+import path from "path"
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 
@@ -6,4 +7,9 @@ import UnoCSS from "unocss/vite";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), UnoCSS()],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
 });
