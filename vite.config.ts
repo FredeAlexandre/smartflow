69dcf728e378f185e3322768c589655f1958f869
Alexandre Frede <alexandre.frede.pro@gmail.com>
2024-07-01 11:58:26 +0200

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -6,4 +6,5 @@ import UnoCSS from "unocss/vite";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), UnoCSS()],
+  base: "https://fredealexandre.github.io/smartflow/",
 });
