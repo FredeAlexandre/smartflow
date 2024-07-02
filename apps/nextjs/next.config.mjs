27557d8891dffdadbd7b2243d07b840e5975ef7b
Alexandre Frede <alexandre.frede.pro@gmail.com>
2024-07-02 12:10:28 +0200

    Format and fix formating

--- apps/nextjs/next.config.mjs
+++ apps/nextjs/next.config.mjs
@@ -1,19 +1,19 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    reactStrictMode: true,
+  reactStrictMode: true,
 
-    transpilePackages: [
-        /**
+  transpilePackages: [
+    /**
         "@smartflow/api",
         "@smartflow/auth",
         "@smartflow/db",
         "@smartflow/ui",
         "@smartflow/validators",
          */
-      ],
+  ],
 
-    eslint: { ignoreDuringBuilds: true },
-    typescript: { ignoreBuildErrors: true },
+  eslint: { ignoreDuringBuilds: true },
+  typescript: { ignoreBuildErrors: true },
 };
 
 export default nextConfig;
