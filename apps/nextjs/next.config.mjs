e80f3a6d0ebba0b3a56ba8484631126e0f4cc513
Alexandre Frede <alexandre.frede.pro@gmail.com>
2024-07-02 11:44:55 +0200

    Add nextjs

---
+++ apps/nextjs/next.config.mjs
@@ -0,0 +1,19 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    reactStrictMode: true,
+
+    transpilePackages: [
+        /**
+        "@smartflow/api",
+        "@smartflow/auth",
+        "@smartflow/db",
+        "@smartflow/ui",
+        "@smartflow/validators",
+         */
+      ],
+
+    eslint: { ignoreDuringBuilds: true },
+    typescript: { ignoreBuildErrors: true },
+};
+
+export default nextConfig;
