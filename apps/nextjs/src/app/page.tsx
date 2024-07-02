787093dec399e4eb25a5acd2c4feafcc54614984
Alexandre Frede <alexandre.frede.pro@gmail.com>
2024-07-02 16:44:11 +0200

    Add shadcn to codebase

--- apps/nextjs/src/app/page.tsx
+++ apps/nextjs/src/app/page.tsx
@@ -1,3 +1,13 @@
+import { Button } from "@smartflow/ui/button";
+
 export default function Home() {
-  return <div>SmartFlow</div>;
+  return (
+    <div className="mt-10 flex flex-col items-center gap-2">
+      <h1 className="text-4xl font-bold">SmartFlow</h1>
+      <div className="grid grid-cols-2 gap-2">
+        <Button variant="outline">Documentation</Button>
+        <Button>Sign up</Button>
+      </div>
+    </div>
+  );
 }
