03a03011562e69d6a952ee30fc7d54f8cbcbc666
Léandre <leandre.sentenac@epitech.eu>
2024-07-03 15:42:31 +0200

    Add project dialog

--- apps/nextjs/src/app/dashboard/_components/project-box.tsx
+++ apps/nextjs/src/app/dashboard/_components/project-box.tsx
@@ -1,17 +1,18 @@
-import React from 'react';
+import React from "react";
 
-// Définir les types des props
-interface ProjectBoxProps {
+export const ProjectBox = ({
+  imageSrc,
+  altText,
+  title,
+}: {
   imageSrc: string;
   altText: string;
   title: string;
-}
-
-export const ProjectBox: React.FC<ProjectBoxProps> = ({ imageSrc, altText, title }) => {
+}) => {
   return (
-    <div className="flex flex-col items-center border-2 rounded-md border-stone-900 w-72 h-52 ml-8 shrink-0">
+    <div className="ml-8 flex h-52 w-72 shrink-0 flex-col items-center rounded-md border-2 border-stone-900">
       <img className="h-4/5 w-full shrink-0" src={imageSrc} alt={altText} />
-      <h3 className="text-stone-900 mt-2 font-bold">{title}</h3>
+      <h3 className="mt-2 font-bold text-stone-900">{title}</h3>
     </div>
   );
 };
