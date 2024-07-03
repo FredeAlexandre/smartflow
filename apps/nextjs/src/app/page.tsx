f9ed5625e853267b1d7add39ee810401151de1d4
thomp <thomas.papaix@epitech.eu>
2024-07-03 11:03:53 +0200

    Merged !

--- apps/nextjs/src/app/page.tsx
+++ apps/nextjs/src/app/page.tsx
@@ -2,12 +2,22 @@ import { Button } from "@smartflow/ui/button";
 
 export default function Home() {
   return (
-    <div className="mt-10 flex flex-col items-center gap-2">
-      <h1 className="text-4xl font-bold">SmartFlow</h1>
-      <div className="grid grid-cols-2 gap-2">
-        <Button variant="outline">Documentation</Button>
-        <Button>Sign up</Button>
+    <div className="w-screen h-screen bg-stone-900">
+    <div className="flex w-full h-20 bg-stone-900 px-8 items-center">
+      <h1 className="text-3xl color-white">Smartflow</h1>
+      <Button className="ml-20 mr-4">Docs</Button>
+      <Button className="mx-4 bg-stone-900">Help</Button>
+      <Button className="mx-4 bg-stone-900">Blog</Button>
+      <div className="flex h-full w-full justify-end items-center">
+        <Button>Sign in</Button>
+        <Button className="ml-4" variant="outline">Sign up</Button>
       </div>
     </div>
+    <div className="justify-center w-full items-center slide-up flex-col flex">
+      <h1 className="w-2/4 mt-30 text-center text-color-white text-7xl font-extrabold bg-gradient-to-r bg-clip-text text-transparent from-gray from-15% via-white via-50% to-gray to-85%">Le meilleur moyen de créer des smart contracts</h1>
+      <p className="mt-10 text-3xl w-2/4 text-center text-color-neutral-400">La solution no-code qui peut vous permetre de crée et deployer des smart contract</p>        
+      <Button className="mt-10">Get started</Button>
+    </div>
+  </div>
   );
 }
