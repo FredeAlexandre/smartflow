9aa4016962397ecd20022c7ca69007fb86cd6de6
thomp <thomas.papaix@epitech.eu>
2024-07-03 10:48:07 +0200

    Squashed commit of the following:
    commit 787093dec399e4eb25a5acd2c4feafcc54614984
    Author: Alexandre Frede <alexandre.frede.pro@gmail.com>
    Date:   Tue Jul 2 16:44:11 2024 +0200
    
        Add shadcn to codebase
    
    commit a2112faf18a5149119137398250d66e90fc1d774
    Author: Alexandre Frede <alexandre.frede.pro@gmail.com>
    Date:   Tue Jul 2 16:11:03 2024 +0200
    
        Add users amount in elysia
    
    commit 35854be1863597294cf568a9c8202f5a57e2375a
    Author: Alexandre Frede <alexandre.frede.pro@gmail.com>
    Date:   Tue Jul 2 16:01:45 2024 +0200
    
        Add drizle db
    
    commit 9642c4a91f90b0a8f1229c8ac9c3a676566560cf
    Author: Alexandre Frede <alexandre.frede.pro@gmail.com>
    Date:   Tue Jul 2 15:57:32 2024 +0200
    
        Add right env variable
    
    commit 4e928ae300c1b569965323c2c1a58fb8f4c47247
    Author: Alexandre Frede <alexandre.frede.pro@gmail.com>
    Date:   Tue Jul 2 15:47:35 2024 +0200
    
        Add db
    
    commit e56a82f931e4abf6fad4302508dee20dad838b21
    Author: Alexandre Frede <alexandre.frede.pro@gmail.com>
    Date:   Tue Jul 2 13:56:01 2024 +0200
    
        Add env variables checks
    
    commit 3a66fd617968b610517b2a301450b62e578c6e36
    Author: Alexandre Frede <alexandre.frede.pro@gmail.com>
    Date:   Tue Jul 2 12:21:01 2024 +0200
    
        Add backend API
    
    commit 27557d8891dffdadbd7b2243d07b840e5975ef7b
    Author: Alexandre Frede <alexandre.frede.pro@gmail.com>
    Date:   Tue Jul 2 12:10:28 2024 +0200
    
        Format and fix formating
    
    commit 8cbc7983af323d0343f85e47963229db6d40bf17
    Author: Alexandre Frede <alexandre.frede.pro@gmail.com>
    Date:   Tue Jul 2 12:04:55 2024 +0200
    
        Fix versions
    
    commit e80f3a6d0ebba0b3a56ba8484631126e0f4cc513
    Author: Alexandre Frede <alexandre.frede.pro@gmail.com>
    Date:   Tue Jul 2 11:44:55 2024 +0200
    
        Add nextjs
    
    commit 491570a7ef2f72a723478c9b2d2e5330994435a5
    Author: Alexandre Frede <alexandre.frede.pro@gmail.com>
    Date:   Tue Jul 2 10:49:43 2024 +0200
    
        Add all the right tooling
    
    commit dcb71ae3e9afa8893422b43043913211629e2a92
    Author: Alexandre Frede <alexandre.frede.pro@gmail.com>
    Date:   Mon Jul 1 14:20:06 2024 +0200
    
        Remove everything
    
    commit f605ab3688a64bebfb364ade267ef8e178bbb8cd
    Author: Alexandre Frede <alexandre.frede.pro@gmail.com>
    Date:   Mon Jul 1 14:00:59 2024 +0200
    
        Update README.md

---
+++ apps/nextjs/eslint.config.js
@@ -0,0 +1,14 @@
+import baseConfig, { restrictEnvAccess } from "@smartflow/eslint-config/base";
+import nextjsConfig from "@smartflow/eslint-config/nextjs";
+import reactConfig from "@smartflow/eslint-config/react";
+
+/** @type {import('typescript-eslint').Config} */
+export default [
+  {
+    ignores: [".next/**"],
+  },
+  ...baseConfig,
+  ...reactConfig,
+  ...nextjsConfig,
+  ...restrictEnvAccess,
+];
---
+++ packages/api/src/index.ts
@@ -0,0 +1,18 @@
+import { Elysia } from "elysia";
+
+import { count } from "@smartflow/db";
+
+import { db } from "@smartflow/db/client";
+
+import { User } from "@smartflow/db/schema";
+
+export const server = new Elysia({ prefix: "/api" })
+  .get("/", () => "Hello Elysia")
+  .get(
+    "/users-amount",
+    async () => {
+        console.log("Hello from elysia")
+        const amount = await db.select({ count: count() }).from(User)
+        return amount[0]?.count
+    },
+  );
