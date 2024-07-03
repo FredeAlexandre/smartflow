f3556a586049d3bc735a08a3c908f9b7caaf211e
thomp <thomas.papaix@epitech.eu>
2024-07-03 13:13:45 +0200

    FIX: landing page

--- apps/nextjs/src/app/page.tsx
+++ apps/nextjs/src/app/page.tsx
@@ -2,22 +2,30 @@ import { Button } from "@smartflow/ui/button";
 
 export default function Home() {
   return (
-    <div className="w-screen h-screen bg-stone-900">
-    <div className="flex w-full h-20 bg-stone-900 px-8 items-center">
-      <h1 className="text-3xl color-white">Smartflow</h1>
-      <Button className="ml-20 mr-4">Docs</Button>
-      <Button className="mx-4 bg-stone-900">Help</Button>
-      <Button className="mx-4 bg-stone-900">Blog</Button>
-      <div className="flex h-full w-full justify-end items-center">
-        <Button>Sign in</Button>
-        <Button className="ml-4" variant="outline">Sign up</Button>
+    <div className=" h-screen w-screen bg-stone-900">
+      <div className="flex h-20 w-full items-center bg-stone-900 px-8">
+        <h1 className="text-3xl text-white">Smartflow</h1>
+        <Button className="ml-20 mr-4">Docs</Button>
+        <Button className="mx-4 bg-stone-900">Help</Button>
+        <Button className="mx-4 bg-stone-900">Blog</Button>
+        <div className="flex h-full w-full items-center justify-end">
+          <Button>Sign in</Button>
+          <Button className="ml-4" variant="outline">
+            Sign up
+          </Button>
+        </div>
+      </div>
+      <div className="slide-up mt-28 flex w-full flex-col items-center justify-center">
+        <h1 className="w-2/4 text-center font-extrabold text-7xl bg-clip-text text-transparent bg-gradient-to-r from-[#808080] from-20% via-white via-50% to-[#808080] to-85% "
+        >
+          Le meilleur moyen de créer des smart contracts
+        </h1>
+        <p className="text-[#d9d9d9] mt-10 w-2/4 text-center text-3xl">
+          La solution no-code qui peut vous permetre de crée et deployer des
+          smart contract
+        </p>
+        <Button className="mt-10">Get started</Button>
       </div>
     </div>
-    <div className="justify-center w-full items-center slide-up flex-col flex">
-      <h1 className="w-2/4 mt-30 text-center text-color-white text-7xl font-extrabold bg-gradient-to-r bg-clip-text text-transparent from-gray from-15% via-white via-50% to-gray to-85%">Le meilleur moyen de créer des smart contracts</h1>
-      <p className="mt-10 text-3xl w-2/4 text-center text-color-neutral-400">La solution no-code qui peut vous permetre de crée et deployer des smart contract</p>        
-      <Button className="mt-10">Get started</Button>
-    </div>
-  </div>
   );
 }
