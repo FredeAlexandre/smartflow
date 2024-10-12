3db8bcb6e0fdb01f548bab12ad3d96ae93260c28
Alexandre Frede <alexandre.frede.pro@gmail.com>
2024-10-12 22:09:09 +0200

    add path alias

--- apps/website/app.config.ts
+++ apps/website/app.config.ts
@@ -1,7 +1,16 @@
 import { defineConfig } from "@tanstack/start/config";
 
+import viteTsConfigPaths from "vite-tsconfig-paths";
+
 export default defineConfig({
   server: {
     preset: "node-server",
+  },
+  vite: {
+    plugins: () => [
+      viteTsConfigPaths({
+        projects: ["./tsconfig.json"],
+      }),
+    ],
   },
 });
