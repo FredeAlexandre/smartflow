da8a3c3f9c0b8b0d4c14ad09527e44e039155038
Alexandre Frede <alexandre.frede.pro@gmail.com>
2024-10-15 12:03:34 +0200

    Add better dev script for pocketbase

--- apps/pocketbase/dev.js
+++ apps/pocketbase/dev.js
@@ -6,6 +6,7 @@ const DOCKER_DATA_VOLUME_NAME = "pb_data";
 const commands = {
   START_CONTAINER: `docker run -it --rm -v ${DOCKER_DATA_VOLUME_NAME}:/pb/pb_data -v ./pb_migrations:/pb/pb_migrations -v ./pb_hooks:/pb/pb_hooks -p 8080:8080 ${DOCKER_IMAGE_NAME}`,
   BUILD_IMAGE: `docker build -t ${DOCKER_IMAGE_NAME} .`,
+  DELETE_IMAGE: `docker rmi ${DOCKER_IMAGE_NAME}`,
   DOES_IMAGE_EXIST: `docker image inspect ${DOCKER_IMAGE_NAME}`,
   CREATE_VOLUME: `docker volume create ${DOCKER_DATA_VOLUME_NAME}`,
   DOES_VOLUME_EXIST: `docker volume inspect ${DOCKER_DATA_VOLUME_NAME}`,
@@ -18,6 +19,16 @@ function runCommand(command) {
   } catch (error) {
     console.error(`Error executing command: ${command}`);
     process.exit(1);
+  }
+}
+
+// Verifier si Docker est lancer
+function isDockerRunning() {
+  try {
+    execSync("docker ps", { stdio: "ignore" });
+    return true;
+  } catch {
+    return false;
   }
 }
 
@@ -41,19 +52,35 @@ function doesVolumeExist() {
   }
 }
 
+//////////////////////////////////////////////////////////////////////////////////////////////////
+
+console.log("Running PocketBase...");
+if (!isDockerRunning()) {
+  console.error("Docker is not running, please start Docker first.");
+  process.exit(1);
+}
+
 // Si le volume n'existe pas, on le crée
 if (!doesVolumeExist()) {
   console.log("Volume pb_data does not exist, creating volume...");
   runCommand(commands.CREATE_VOLUME);
+} else {
+  console.log("Volume pb_data exists, skipping volume creation.");
 }
 
 // Si l'image existe, on lance le container, sinon on la build d'abord
 if (doesImageExist()) {
-  console.log("Image pocketbase exists, starting container...");
-  runCommand(commands.START_CONTAINER);
-} else {
-  console.log("Image pocketbase does not exist, building image...");
-  runCommand(commands.BUILD_IMAGE);
-  console.log("Image built successfully, starting container...");
-  runCommand(commands.START_CONTAINER);
+  console.log("Image pocketbase exists, deleting...");
+  runCommand(commands.DELETE_IMAGE)
 }
+
+console.log("Building image...");
+runCommand(commands.BUILD_IMAGE);
+console.log("Image built successfully, starting container...");
+runCommand(commands.START_CONTAINER);
+
+// On process exit delete the image
+process.on("exit", () => {
+  console.log("Deleting image...");
+  runCommand(commands.DELETE_IMAGE);
+});
