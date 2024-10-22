57e345d2a2b22c30a048ba4be268784808736efa
Ekhalios <leandre.sentenac@epitech.eu>
2024-10-22 18:18:17 +0200

    Add basic contract/functions

--- apps/website/app/components/draggableContract.tsx
+++ apps/website/app/components/draggableContract.tsx
@@ -1,6 +1,23 @@
-import { useDraggable } from "@dnd-kit/core";
+import {
+  DndContext,
+  type DragEndEvent,
+  KeyboardSensor,
+  PointerSensor,
+  closestCenter,
+  useDraggable,
+  useSensor,
+  useSensors,
+} from "@dnd-kit/core";
+import {
+  SortableContext,
+  arrayMove,
+  sortableKeyboardCoordinates,
+  useSortable,
+  verticalListSortingStrategy,
+} from "@dnd-kit/sortable";
 import type React from "react";
-import { useState } from "react";
+import { useRef, useState } from "react";
+import { DraggableFunction } from "./draggableFunction";
 import { Input } from "./ui/input";
 
 interface DraggableProps {
@@ -13,6 +30,47 @@ export function DraggableContract({ id, content, styles }: DraggableProps) {
   const { attributes, listeners, setNodeRef, transform } = useDraggable({
     id,
   });
+
+  const contractNameRef = useRef<HTMLInputElement>(null);
+
+  const sensors = useSensors(
+    useSensor(PointerSensor),
+    useSensor(KeyboardSensor, {
+      coordinateGetter: sortableKeyboardCoordinates,
+    }),
+  );
+
+  function handleDragEnd(event: DragEndEvent) {
+    const { active, over } = event;
+    if (active?.id !== over?.id) {
+      setItems((prev) => {
+        const activeIndex = prev.findIndex((item) => item.id === active?.id);
+        const overIndex = prev.findIndex((item) => item.id === over?.id);
+        return arrayMove(prev, activeIndex, overIndex);
+      });
+    }
+  }
+
+  const functions = [
+    {
+      id: "1",
+      title: "Function1",
+    },
+    {
+      id: "2",
+      title: "Function2",
+    },
+    {
+      id: "3",
+      title: "Function3",
+    },
+    {
+      id: "4",
+      title: "Function4",
+    },
+  ];
+
+  const [items, setItems] = useState(functions);
 
   const [inputText, setInputText] = useState(content);
 
@@ -31,27 +89,52 @@ export function DraggableContract({ id, content, styles }: DraggableProps) {
       id={id}
       ref={setNodeRef}
       style={{ ...style, ...styles }}
-      className="flex h-12 w-80 place-items-center rounded-lg bg-white font-medium text-black text-sm"
-      {...listeners}
+      className="flex h-fit w-96 flex-col rounded-lg border-2 border-black bg-white font-medium text-black text-sm"
       {...attributes}
     >
-      <input
-        type="text"
-        value={inputText}
-        onChange={handleInputChange}
-        className="ml-4"
-        onKeyDown={(e) => {
-          // Empêche l'événement de drag lors de la saisie
-          e.stopPropagation();
-        }}
-      />
-      <div
-        style={{ cursor: "grab" }}
-        {...listeners}
-        className="mr-4 flex w-full justify-end"
-      >
-        {/* Zone de drag, peut être une icône ou un simple texte */}
-        <span>:::</span>
+      <div className="mt-4 flex h-12 w-full flex-row items-center justify-start">
+        <input
+          type="text"
+          ref={contractNameRef}
+          value={inputText}
+          onChange={handleInputChange}
+          className="ml-4"
+          onKeyDown={(e) => {
+            e.stopPropagation();
+            if (e.key === "Enter") {
+              if (contractNameRef.current) {
+                contractNameRef.current.blur();
+              }
+            }
+          }}
+        />
+        <div
+          style={{ cursor: "grab" }}
+          {...listeners}
+          className="mr-4 flex w-full justify-end"
+        >
+          {/* Zone de drag, peut être une icône ou un simple texte */}
+          <span>:::</span>
+        </div>
+      </div>
+      <div className="my-8 flex justify-center">
+        <DndContext
+          sensors={sensors}
+          collisionDetection={closestCenter}
+          onDragEnd={handleDragEnd}
+        >
+          <SortableContext items={items} strategy={verticalListSortingStrategy}>
+            <ul>
+              {items.map((item) => (
+                <DraggableFunction
+                  id={item.id}
+                  key={item.id}
+                  content={item.title}
+                />
+              ))}
+            </ul>
+          </SortableContext>
+        </DndContext>
       </div>
     </div>
   );
--- apps/website/app/components/draggableFunction.tsx
+++ apps/website/app/components/draggableFunction.tsx
@@ -1,6 +1,7 @@
 import { useDraggable } from "@dnd-kit/core";
+import { useSortable } from "@dnd-kit/sortable";
 import type React from "react";
-import { useState } from "react";
+import { useRef, useState } from "react";
 import { Input } from "./ui/input";
 
 interface DraggableProps {
@@ -10,9 +11,18 @@ interface DraggableProps {
 }
 
 export function DraggableFunction({ id, content, styles }: DraggableProps) {
-  const { attributes, listeners, setNodeRef, transform } = useDraggable({
-    id,
+  const {
+    attributes,
+    listeners,
+    setNodeRef,
+    setActivatorNodeRef,
+    transform,
+    transition,
+  } = useSortable({
+    id: id,
   });
+
+  const functionNameRef = useRef<HTMLInputElement>(null);
 
   const [inputText, setInputText] = useState(content);
 
@@ -31,18 +41,23 @@ export function DraggableFunction({ id, content, styles }: DraggableProps) {
       id={id}
       ref={setNodeRef}
       style={{ ...style, ...styles }}
-      className="flex h-12 w-80 place-items-center rounded-lg bg-white font-medium text-black text-sm"
-      {...listeners}
+      className="mb-4 flex h-12 w-80 place-items-center rounded-lg border border-black bg-white font-medium text-black text-sm"
       {...attributes}
     >
       <input
         type="text"
+        ref={functionNameRef}
         value={inputText}
         onChange={handleInputChange}
         className="ml-4"
         onKeyDown={(e) => {
           // Empêche l'événement de drag lors de la saisie
           e.stopPropagation();
+          if (e.key === "Enter") {
+            if (functionNameRef.current) {
+              functionNameRef.current.blur();
+            }
+          }
         }}
       />
       <div
